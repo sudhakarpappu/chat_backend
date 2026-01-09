fd3b55d3a6c3a21eeb645129addcfed4a8bdcacb
sudhakarpappu <62554341+sudhakarpappu@users.noreply.github.com>
2026-01-09 20:24:49 +0530

    change in app

--- app/main.py
+++ app/main.py
@@ -1,16 +1,15 @@
 from fastapi import FastAPI
 from app.routes import users, messages
-
 from app.routes.websocket import router as websocket_router
 
 app = FastAPI()
+
+# ✅ WebSocket router
 app.include_router(websocket_router)
 
-
-
+# ✅ REST routers
 app.include_router(users.router)
 app.include_router(messages.router)
-app.include_router(websocket.router)
 
 @app.get("/")
 def health():
